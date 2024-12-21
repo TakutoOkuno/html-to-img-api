d3755a183b9801511d69f9efa675a1561bdff443
Takuto Okuno <takuto.okuno.ku0823@gmail.com>
2024-12-21 18:06:39 +0900

    feat: specify executable path from github

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ app.post('/convert', async (req, res) => {
     const outputFileName = 'coverageReport.png';
     const outputImagePath = `${outputDir}${outputFileName}`;
 
-    const executablePath = await chromium.executablePath();
+    const executablePath = await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar');
     console.log('Executable Path:', executablePath);
 
     // Chromiumバイナリの存在を確認
